669cf05807abea85f84fe1b848041d9f357c1a86
Evgeny <evkoldin@mail.ru>
2020-04-21 15:57:34 +0300

    Настройки конфигураций

--- build/base-config.js
+++ build/base-config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, '../dist'),
+	},
+};
