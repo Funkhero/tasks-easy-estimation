7fb5af56fd1e02fb947bf5686cd4edfbbb108caa
Evgeny <evkoldin@mail.ru>
2020-05-28 11:26:07 +0300

    Доработки динамического роутера

--- src/router.js
+++ src/router.js
@@ -1,38 +1,46 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import { requireAll } from './utilities/index';
+
 Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'index',
-    component: () => import(/* webpackChunkName: "indexPage" */ './views/Index.vue'),
-  },
-  {
-    path: '/assessment',
-    component: () => import(/* webpackChunkName: "assessmentPage" */ './views/assessment/Index.vue'),
-    children: [
-      {
-        path: '',
-        name: 'assessments-list',
-        component: () => import(/* webpackChunkName: "assessmentPage" */ './views/assessment/List.vue'),
-      },
-      {
-        path: 'room',
-        name: 'assessments-room',
-        component: () => import(/* webpackChunkName: "assessmentPage" */ './views/assessment/Room.vue'),
-      },
-    ],
-  },
-  {
-    path: '*',
-    component: () => import(/* webpackChunkName: "404" */ './views/404.vue'),
-    meta: {
-      layout: 'Error',
-    },
-  },
-];
+const routes = [];
+let route404 = null;
+
+function registerRoute(path, component) {
+  const route = {
+    path,
+    component,
+    name: component.routeName,
+    meta: component.routeMeta,
+  };
+
+  if (route.path === '/404') {
+    route.path = '*';
+    if (!route.meta) route.meta = { statusCode: 404 };
+    else if (!route.meta.statusCode) route.meta.statusCode = 404;
+    route404 = route;
+  } else {
+    if (route.path === '') route.path = '/';
+    if (component.routes) route.children = component.routes;
+    route.props = component.routeProps === undefined ? true : component.routeProps;
+
+    routes.push(route);
+  }
+}
+
+requireAll(require.context('./views/', true, /^(?:(?!\/?_).)+\.(vue|js)$/), (component, name) => {
+  const filePath = name.substr(1).replace(/(\/Index)?\.[a-zA-Z0-9]+$/, '');
+  component.routeFilePath = filePath;
+  if (component.routePath) {
+    (Array.isArray(component.routePath) ? component.routePath : [component.routePath]).forEach((path) => {
+      registerRoute(`${filePath}/${path}`, component);
+    });
+  } else registerRoute(filePath, component);
+});
+
+if (route404) routes.push(route404);
 
 const router = new Router({
   routes,
--- src/views/assessment/Index.vue
+++ src/views/assessment/Index.vue
@@ -7,8 +7,16 @@
 </template>
 
 <script>
+import Room from './_Room';
+import List from './_List';
+
 export default {
   name: 'Assessment',
+  routeName: 'assessment',
+  routes: [
+    { name: 'assessments-list', path: '', component: List },
+    { name: 'assessments-room', path: 'room/:id', component: Room },
+  ],
 };
 </script>
 
